7400c5095a6c8351a1488a3428ccab5327ee54e3
Jenny Cunningham <jenncunn131@gmail.com>
2020-09-07 15:41:21 -0400

    Javascript additions
    Adding in the js folder and data.js file

---
+++ app/public/js/data.js
@@ -0,0 +1,27 @@
+
+var data = []
+[
+  {
+    "id": 31,
+    "name": "Adam"
+  },
+  {
+    "id": 32,
+    "name": "Brittany"
+  },
+  {
+    "id": 35,
+    "name": "Clara"
+  }
+];
+
+console.log(data);
+
+data.forEach( function(item) {
+  console.log(item);
+
+  // TODO: created all of the divs in the patient pane
+  // Get the element leftPanel
+  // $('#leftPanel');
+  // el.append( new element)
+});
