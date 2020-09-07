4d8e6b4d7c7682b55523f7eeac08e5ddd38bfb38
Luke Pillarella <lpillarella.tmr@gmail.com>
2020-09-07 16:00:05 -0400

    Error Fix

---
+++ app/public/js/data.js
@@ -0,0 +1,19 @@
+var data = [
+    {"id": 31,
+      "name": "Adam"},
+
+    {"id": 32,
+      "name": "Brittany"},
+
+    {"id":33,
+      "name": "Luke"}
+];
+
+console.log(data);
+
+data.forEach(function(item){
+  console.log(item);
+
+  //Get left-hand pane
+  //Add this object as div to the cardPanel
+});
