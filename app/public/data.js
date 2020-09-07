ac93749073db1cde578fab0ca992bd4726ddd393
Ethan Parsley <ecparsle@iu.edu>
2020-09-07 14:46:17 -0400

    JS Data Added
    In Class, 9/7/2020 (using the basics of javascript to better understand how it works)

---
+++ app/public/data.js
@@ -0,0 +1,22 @@
+
+var data = [
+{
+"id": 31,
+"name": "Adam"
+}
+{
+"id": 32,
+"name": "Brittany"
+}
+{
+"id": 35,
+"name": "Clara"
+}
+
+];
+
+console.log(data);
+
+data.forEach( function(item) {
+  console.log(item)
+});
