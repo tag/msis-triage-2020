4fae0b30427f685231d155819c746ad37bff135f
Altman20 <70535952+Altman20@users.noreply.github.com>
2020-09-07 15:37:50 -0400

    started js

---
+++ app/public/js/data.js
@@ -0,0 +1,19 @@
+var data = {
+  {
+    "id": 31;
+    "name": Michael;
+  }
+  {
+    "id": 32;
+    "name": Kendel;
+  }
+}
+
+console.log(data[0]);
+
+data.forEach(function(item) => {
+  console.log(item);
+
+  //get left hand pane
+  
+});
