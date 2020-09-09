57426231306d7f25f457b9131e7752bb65257b0b
smatharu7 <smatharu77@gmail.com>
2020-09-09 14:50:53 -0400

    9/9/20 class

---
+++ app/public/js/data.js
@@ -0,0 +1,33 @@
+[
+  {
+    "id":31,
+    "name":"Adam"
+  },
+  {
+    "id":32,
+    "name":"Brittany"
+  },
+  {
+    "id":33,
+    "name":"Clara"
+  },
+
+];
+
+console.log(data);
+console.log(data[0]);
+console.log(data[1]);
+console.log(data[2]);
+console.log(data[3]);
+
+//function for going through an array
+data.forEach(function(item){
+  console.log(item)
+});
+
+//TODO: create all of the divs in the patient page
+//get the element in the left panel
+//var el $('#leftPanel');
+// el.append( new element )
+
+)
---
+++ app/public/js/index.app.js
@@ -0,0 +1,11 @@
+var cardPaneLeft = new Vue({
+  el: '#cardPaneLeft',
+  data: {
+    message: 'Hello Vue!'',
+    onePatient:{
+      "patientGuid": "SOME-REALLY-LONG-1234",
+      "firstName": "Sylvia",
+      "lastName": "Hernandez"
+    }
+  }
+})
