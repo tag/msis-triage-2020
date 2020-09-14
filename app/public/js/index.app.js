fedf9c9ed046f7975b22bc8d48a3a7c585d4c0f2
Tom G <tomg@byu.net>
2020-09-14 01:33:48 -0400

    v-for example

--- app/public/js/index.app.js
+++ app/public/js/index.app.js
@@ -2,13 +2,40 @@ var app = new Vue({
   el: '#cardPaneLeft',
   data: {
     message: 'Hello Vue!',
-    onePatient: {
+    onePt: {
       "patientGuid": "SOME-REALLY-LONG-1234",
       "firstName": "Sylvia",
       "lastName": "Hernandez",
       "dob": "2012-09-01",
       "sexAtBirth": "F",
       "priority": "critical"
-    }
+    },
+    ptList: [
+      {
+        "patientGuid": "SOME-REALLY-LONG-1234",
+        "firstName": "Sylvia",
+        "lastName": "Hernandez",
+        "dob": "2012-09-01",
+        "sexAtBirth": "F",
+        "priority": "critical"
+      },
+      {
+        "patientGuid": "SOME-REALLY-SHORT-5678",
+        "firstName": "Vish",
+        "lastName": "Balasubramanian",
+        "dob": "1950-12-15",
+        "sexAtBirth": "M",
+        "priority": "urgent"
+      },
+      {
+        "patientGuid": "SOME-UNIQUE-ABCDE1",
+        "firstName": "John",
+        "lastName": "Doe",
+        "dob": "1997",
+        "sexAtBirth": "M",
+        "priority": "minor"
+      }
+    ],
+    user: {}
   }
 })
