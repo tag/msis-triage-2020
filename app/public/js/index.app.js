f0c28e2f5d216397ebec41003bcfa6ddbfe5afe8
Tom G <tomg@byu.net>
2020-09-14 20:07:03 -0400

    Now w/ `fetch()` and example `onclick` handler.

--- app/public/js/index.app.js
+++ app/public/js/index.app.js
@@ -10,32 +10,24 @@ var app = new Vue({
       "sexAtBirth": "F",
       "priority": "critical"
     },
-    ptList: [
-      {
-        "patientGuid": "SOME-REALLY-LONG-1234",
-        "firstName": "Sylvia",
-        "lastName": "Hernandez",
-        "dob": "2012-09-01",
-        "sexAtBirth": "F",
-        "priority": "critical"
-      },
-      {
-        "patientGuid": "SOME-REALLY-SHORT-5678",
-        "firstName": "Vish",
-        "lastName": "Balasubramanian",
-        "dob": "1950-12-15",
-        "sexAtBirth": "M",
-        "priority": "urgent"
-      },
-      {
-        "patientGuid": "SOME-UNIQUE-ABCDE1",
-        "firstName": "John",
-        "lastName": "Doe",
-        "dob": "1997",
-        "sexAtBirth": "M",
-        "priority": "minor"
-      }
-    ],
-    user: {}
+    ptList: [],
+    user: {},
+    times: 0
+  },
+  methods: {
+    yell(msg) {
+      this.times = this.times + 1;
+      var msg = "Clicked " +this.times+ " times";
+      alert(msg);
+    }
+  },
+  created() {
+    fetch("dummy/pt-list.php")
+    .then( response => response.json() )
+    .then( json => {
+      this.ptList = json;
+
+      console.log(json)}
+    );
   }
 })
