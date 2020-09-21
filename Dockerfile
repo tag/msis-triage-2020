e84198e66debb3fdc9e42307de19023914c7f4bb
smatharu7 <smatharu77@gmail.com>
2020-09-21 14:53:13 -0400

    9/21

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
+
 FROM php:7.4-apache
 
-LABEL maintainer="Tom Gregory"
-COPY app/public/ /var/www/html
+LABEL maintainer="Simran Matharu"
+
+COPY app /srv/app
+
+COPY docker/apache/vhost.conf /etc/apache2/sites-available/000-default.conf
--- app/public/js/index.app.js
+++ app/public/js/index.app.js
@@ -1,14 +1,72 @@
 var app = new Vue({
-  el: '#cardPaneLeft',
+  el: '#triagePage',
   data: {
-    message: 'Hello Vue!',
-    onePatient: {
-      "patientGuid": "SOME-REALLY-LONG-1234",
-      "firstName": "Sylvia",
-      "lastName": "Hernandez",
-      "dob": "2012-09-01",
-      "sexAtBirth": "F",
-      "priority": "critical"
+    ptList: [],
+    activePt: null,
+    triageForm: {
+      priority: null,
+      symptoms: ''
+    },
+    //create variables for each form field and bind using v-model
+    newPtForm:{
     }
+  },
+  computed: {
+    activePtName() {
+      return this.activePt ? this.activePt.lastName + ', ' + this.activePt.firstName : ''
+    }
+  },
+  methods: {
+    newPt(){
+      return{
+          //returns a brand new empty object
+          firstName:'',
+          lastName:'',
+          dob:'',
+          sexAtBirth:'',
+
+      }
+    },
+    submitTriageForm( evt ) {
+      console.log("Form submitted!");
+
+      this.triageForm.pt = this.activePt;
+      console.log(this.triageForm);
+
+    },
+    handleCreatePatient(evt){
+
+      evt.preventDefault(); //redundant, because we prevent it in the html file
+      //ToDo: actually creat the pt
+    //
+    //   // fetch(url,){
+    //     method:"POST",
+    //     data:
+    // })
+    // //POST
+    console.log("Creating");
+    console.log(this.newPtForm);
+
+    //when you hit submit, it'll appear on the list on the page whether or not it gets added to the databse
+    this.ptList.push(this.newPtForm);
+
+    // takes variable attached to the form and points it somewhere else
+    this.newPtForm = this.newPt();
+
+    }
+  },
+  //function that is a sibling of el and data
+  created(){
+    //fetch the url. If you can and fetch gets resolved, then run a function. But if you can't, then run the Error Function.
+    //making HTTP request to webserver to retrieve pt-list
+    fetch("dummy/pt-list.php")
+    .then( response => response.json())
+    //returns JSONified response as part of promise
+    //above is shortcut way of writing .then ( function(response) {return response.json()})
+    .then( json => { //second promise
+          //this.project = json;
+          this.ptList = json; //assigns json object as the variable ptList in application
+          console.log(json)}
+    );
   }
 })
