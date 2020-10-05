420f93695304fef81ce3b493e74f076ed85fd846
kengibso <kengibso@iu.edu>
2020-10-05 19:35:04 -0400

    Try 2- Tom's changes through Week 6

--- app/public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// This is a sample file.
-print "hello";
-
-// The following function is built-in to PHP, and trader_will
-// cause all of the PHP settings to display. Uncomment to run.
-
-//phpinfo();
---
+++ app/public/js/index.app.js
@@ -0,0 +1,68 @@
+var app = new Vue({
+  el: '#triagePage',
+  data: {
+    ptList: [],
+    activePt: null,
+    triageForm: {
+      priority: null,
+      symptoms: ''
+    },
+    newPtForm: {}
+  },
+  computed: {
+    activePtName() {
+      return this.activePt ? this.activePt.lastName + ', ' + this.activePt.firstName : ''
+    }
+  },
+  methods: {
+    newPtData() {
+      return {
+        firstName: "",
+        lastName: "",
+        dob: "",
+        sexAtBirth: ""
+      }
+    },
+    handleNewPtForm( evt ) {
+      // evt.preventDefault();  // Redundant w/ Vue's submit.prevent
+
+      // TODO: Validate the data!
+
+      fetch('api/records/post.php', {
+        method:'POST',
+        body: JSON.stringify(this.newPtForm),
+        headers: {
+          "Content-Type": "application/json; charset=utf-8"
+        }
+      })
+      .then( response => response.json() )
+      .then( json => {
+        console.log("Returned from post:", json);
+        // TODO: test a result was returned!
+        this.ptList.push(json[0]);
+      });
+
+      console.log("Creating (POSTing)...!");
+      console.log(this.newPtForm);
+
+      this.newPtForm = this.newPtData();
+    },
+    handleTriageForm( evt ) {
+      console.log("Form submitted!");
+
+      this.triageForm.pt = this.activePt;
+      console.log(this.triageForm);
+
+    }
+  },
+  created() {
+    fetch("api/records/")
+    .then( response => response.json() )
+    .then( json => {
+      this.ptList = json;
+
+      console.log(json)}
+    );
+    this.newPtForm = this.newPtData();
+  }
+})
